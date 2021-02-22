672e497f5a213a9b2b0636349449e7b8c3706bc0
Richard Negron <richard.negron.257@gmail.com>
2021-02-22 16:52:17 -0500

    PeliculasActores y PeliculasGeneros, muchos a muchos

--- PeliculasAPI/ApplicationDBContext.cs
+++ PeliculasAPI/ApplicationDBContext.cs
@@ -13,8 +13,20 @@ namespace PeliculasAPI
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PeliculasActores>()
+                .HasKey(x => new { x.ActorId, x.PeliculaId });
+            modelBuilder.Entity<PeliculasGeneros>()
+                .HasKey(x => new { x.GeneroId, x.PeliculasId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
         public DbSet<Genero> Generos { get; set; }
         public DbSet<Actor> Actores { get; set; }
         public DbSet<Pelicula> Peliculas { get; set; }
+        public DbSet<PeliculasActores> PeliculasActores { get; set; }
+        public DbSet<PeliculasGeneros> PeliculasGeneros { get; set; }
     }
 }
