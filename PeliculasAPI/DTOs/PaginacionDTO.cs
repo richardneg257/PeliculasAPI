5ec10a43cd2d18d52f8ad56c1cb6ca7b15e489e8
Richard Negron <richard.negron.257@gmail.com>
2021-02-22 09:15:08 -0500

    Paginacion, HttpContextExtension y QueryableExtensions

---
+++ PeliculasAPI/DTOs/PaginacionDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.DTOs
+{
+    public class PaginacionDTO
+    {
+        public int Pagina { get; set; } = 1;
+        public int CantidadRegistrosPorPagina
+        {
+            get => cantidadRegistrosPorPagina;
+            set
+            {
+                cantidadRegistrosPorPagina = (value > cantidadMaximaRegistrosPorPagina) ? cantidadMaximaRegistrosPorPagina : value;
+            }
+        }
+
+        private int cantidadRegistrosPorPagina { get; set; } = 10;
+        private readonly int cantidadMaximaRegistrosPorPagina = 50;
+    }
+}
---
+++ PeliculasAPI/Helpers/HttpContextExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Helpers
+{
+    public static class HttpContextExtension
+    {
+        public async static Task InsertarParametrosPaginacion<T>(this HttpContext httpContext, IQueryable<T> queryable, int cantidadRegistrosPorPagina)
+        {
+            double cantidad = await queryable.CountAsync();
+            double cantidadPaginas = Math.Ceiling(cantidad / cantidadRegistrosPorPagina);
+            httpContext.Response.Headers.Add("CantidadPaginas", cantidadPaginas.ToString());
+        }
+    }
+}
